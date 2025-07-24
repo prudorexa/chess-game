d9b37031cbf59a2f976cd9ed97c76fb72689846a
prudorexa <mathuprudence24@gmail.com>
2025-07-24 14:28:55 +0300

    Add chessly

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-black text-white flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-1 mt-4">♟ Chess Game</h1>
+      <h1 className="text-3xl font-bold mb-1 mt-4">♟ Chessly</h1>
       <ChessBoard />
       {/* <ChessGame /> */}
     </div>
